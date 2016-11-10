d272712236cc08769ddb0bfbfc761d36cccae777
SophiaY <Sophiyah@users.noreply.github.com>
2016-11-10 17:17:06 -0500

    Added header and source Files
    Turn on Red LED in main file

---
+++ Elisa3ECE6970/default/dep/ir_remote_control.o.d
@@ -0,0 +1,49 @@
+ir_remote_control.o: ../ir_remote_control.c ../ir_remote_control.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr\io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom2560.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iomxx0_1.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr\interrupt.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h ../variables.h \
+  ../constants.h ../leds.h
+
+../ir_remote_control.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr\io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom2560.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iomxx0_1.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr\interrupt.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+../variables.h:
+
+../constants.h:
+
+../leds.h:
