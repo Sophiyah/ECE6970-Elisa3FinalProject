8bd4f1c01cd2eca5a8652cc5829f8a0bb3201bdf
SophiaY <Sophiyah@users.noreply.github.com>
2016-11-19 17:27:04 -0500

    created new folder using advanced header and source files

---
+++ Elisa3ECE6970Comm/Elisa3ECE6970.c
@@ -0,0 +1,22 @@
+
+#include <avr\io.h>
+#include <avr\interrupt.h>
+
+#include <stdlib.h>
+#include "variables.h"
+#include "utility.h"
+#include "speed_control.h"
+#include "nRF24L01.h"
+#include "behaviors.h"
+#include "sensors.h"
+#include "irCommunication.h"
+#include "motors.h"
+
+int main(void) {
+
+	while(1) {
+
+	}
+
+
+}
